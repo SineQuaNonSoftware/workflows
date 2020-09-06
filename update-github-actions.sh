7ef3b2070139fa427c0c9de560650afce8a276bb
Clément Goubert <clement@goubert.me>
2020-09-06 16:08:17 +0200

    Better script

--- update-github-actions.sh
+++ update-github-actions.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-WF_DIR='./.github/workflows'
-WF_TYPE=$1
+WF_DIR="${PWD}/.github/workflows"
+WF_TYPE=${1}
 HOOK_DIR=$(dirname $0)
 [[ ! -e ${WF_DIR} ]] && mkdir -p ${WF_DIR}
-diff -q ${HOOK_DIR}/${WF_TYPE}.yml ${WF_DIR}/${WF_TYPE}.yml
-DIFFRET=$?
+if [[ -e ${WF_DIR}/${WF_TYPE}.yml ]]
+then
+  diff -q ${HOOK_DIR}/${WF_TYPE}.yml ${WF_DIR}/${WF_TYPE}.yml
+  DIFFRET=$?
+else
+  echo "No workflow file"
+  DIFFRET=1
+fi
 if [[ ${DIFFRET} -ne 0 ]]
 then
     cp -f ${HOOK_DIR}/${WF_TYPE}.yml ${WF_DIR}/${WF_TYPE}.yml
