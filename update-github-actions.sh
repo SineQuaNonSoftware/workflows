a1ccd5dd8ea33d901ec7e81e1ad8d0003f11152a
Clément Goubert <clement@goubert.me>
2020-09-06 13:58:44 +0200

    A script is better

--- update-github-actions.sh
+++ update-github-actions.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 set -e
+ls
+WF_DIR='./.github/workflows'
+WF_TYPE=$1
+[[ ! -e ${WF_DIR} ]] && mkdir -p ${WF_DIR}
+cd ${WF_DIR}
+curl https://github.com/SineQuaNon/workfows/$WF_TYPE.yml > $WF_TYPE.yml.tmp
+DIFF_RET=$(diff -q $WF_TYPE.yml $WF_TYPE.yml.tmp)
+if [[ ${DIFF_RET} -ne 0]]
+then
+    mv -f ${WF_TYPE}.yml.tmp ${WF_TYPE}.yml
+else
+    rm -f ${WF_TYPE}.yml.tmp
+fi
+return $DIFF_RET
 
-[! -e 
+
